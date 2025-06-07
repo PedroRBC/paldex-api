ed081f9968a3f1ff3911832d14f79b7f07e2afe6
Pedrorbc <pedrinhorbc@gmail.com>
2025-06-07 17:41:58 +0000

    refactor: Use vercel Recomended File Reader

--- api/_pkg/init.go
+++ api/_pkg/init.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 // Pals is the global slice containing all Pal data
@@ -18,8 +19,16 @@ func init() {
 
 // Initialize loads the pals data from the JSON file
 func Initialize() error {
+	// Get current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get working directory: %v", err)
+	}
+
+	// Build full path to JSON file
+	jsonPath := filepath.Join(cwd, "api", "pals.json")
 	// Read and parse the JSON file
-	data, err := os.ReadFile("api/pals.json")
+	data, err := os.ReadFile(jsonPath)
 	if err != nil {
 		return fmt.Errorf("error reading pals.json: %v", err)
 	}
